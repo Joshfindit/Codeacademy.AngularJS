792b245d20dbda4e4dc086fba54be0420b6f61b8
Josh <linkingparents@gmail.com>
2015-07-25 12:33:21 -0300

    13

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -19,4 +19,31 @@ app.controller('MainController', ['$scope', function($scope) {
     developer: 'Armando P.',
     price: 1.99
   };
+
+  $scope.apps = [ 
+  { 
+    icon: 'img/move.jpg', 
+    title: 'MOVE', 
+    developer: 'MOVE, Inc.', 
+    price: 0.99 
+  }, 
+  { 
+    icon: 'img/shutterbugg.jpg', 
+    title: 'Yesterday', 
+    developer: 'Plato', 
+    price: 2.99 
+  },
+  { 
+    icon: 'img/shutterbugg.jpg', 
+    title: 'tomorrow', 
+    developer: 'today', 
+    price: 0.10 
+  },
+  { 
+    icon: 'img/shutterbugg.jpg', 
+    title: 'Shutterbugg', 
+    developer: 'Chico Dusty', 
+    price: 2.99 
+  } 
+  ]
 }]);
