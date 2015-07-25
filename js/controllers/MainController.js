5c0c858a265f5e712ffc960bcd3db6179a5d3e0a
Josh <linkingparents@gmail.com>
2015-07-25 00:19:09 -0300

    3

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,7 @@
 app.controller('MainController', ['$scope', function($scope) { 
   $scope.title = 'Silly Goose';
   $scope.promo = "On sale yesterday!";
+  $scope.product = new Object();
+  $scope.product.name = 'The Book of Trees';
+  $scope.product.price = 19;
 }]);
